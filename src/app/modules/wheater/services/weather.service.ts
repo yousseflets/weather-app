b5c6972d85b2e2bf80f5d50826538ad70655bb0d
yousseflets <leticiayoussef.si@gmail.com>
2026-01-07 23:23:58 -0300

    first commit

---
+++ src/app/modules/wheater/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiKey = '3be03b1bebad83b3698e9fb4e2c528b9';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
